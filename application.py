c8cdbe35a2b5cf390a02eb0ea42729c7f7e6a814
sivaskr <veepuruslakshmi@gmail.com>
2016-06-08 20:39:12 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = flask.Flask(__name__)
 
 @application.route('/')
 def hello():
- return 'Hello, World! This is sample Python Web Applicationnew'
+ return 'Hello, World! This is sample Python Web Applicationnewnewapp'
 
 if __name__ == '__main__':
  application.run(host='0.0.0.0',debug=True)
